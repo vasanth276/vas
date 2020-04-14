a4831b75fa57c7382131828b3d44a4a218912f09
vasanth276 <63463535+vasanth276@users.noreply.github.com>
2020-04-14 14:10:45 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,16 @@
-#include <stdio.h>
-
 int main()
 {
-	int i,j,mat[3][3],transposed_mat[3][3];
-    printf("\n enter the elements of the matrix");
-    printf("**************");
-    for(i=0;i<3;i++)
+	int num[10],i,n,sum=0;
+    float avg;
+    printf("enter the number of elements:");
+    scanf("%d",& n);
+    printf("enter %d values\n",n);
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<3;j++)
-        {
-            printf("\nmat[%d][%d]=",i,j);
-            scanf("%d",& mat[i][j]);
-        }
+        scanf("%d",& num[i]);
+        sum=sum+num[i];
     }
-    printf("\nthe elements of the matrix are");
-    printf("\n*****************");
-    for(i=0;i<3;i++)
-    {
-        printf("\n");
-        for(j=0;j<3;j++)
-            printf("\t%d",mat[i][j]);
-    }
-    for(i=0;i<3;i++)
-    {
-        for(j=0;j<3;j++)
-        transposed_mat[i][j]=mat[j][i];
-    }
-    printf("\nthe elements of the transposed matrix are");
-    printf("\n");
-    for(i=0;i<3;i++)
-    {
-        printf("\n");
-        for(j=0;j<3;j++)
-            printf("\t%d",transposed_mat[i][j]);
-    }
+    avg=sum*1.0/n;
+    printf("average=%f",avg);
     return 0;
 }
